d2424241af7c3279cd0a3467f033b0efcd1cbd24
yaoleinside <yaole@yaoledeMacBook-Pro.local>
2016-10-08 11:47:04 +0800

    Fisrt

---
+++ zhihudaily/zhihudaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhihudaily
+//
+//  Created by 姚乐 on 16/10/8.
+//  Copyright © 2016年 yaole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ zhihudaily/zhihudaily/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhihudaily
+//
+//  Created by 姚乐 on 16/10/8.
+//  Copyright © 2016年 yaole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
